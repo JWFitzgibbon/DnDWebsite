ec408d694afb747791c04abcd19192892429aff2
JWFitzgibbon <123597843+JWFitzgibbon@users.noreply.github.com>
2023-03-07 16:02:07 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    static displayName = App.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { characters: [], abilities: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateCharacterData();
+        this.populateAbilityData();
+    }
+
+    static renderTable(characters, abilities) {
+        return (
+            <><table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Class</th>
+                        <th>Archetype</th>
+                        <th>Level</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {characters && characters.map(character => <tr key={character.id}>
+                        <td>{character.name}</td>
+                        <td>{character.class}</td>
+                        <td>{character.archetype}</td>
+                        <td>{character.level}</td>
+                    </tr>
+                    )}
+                </tbody>
+            </table>
+
+                <table className='table table-striped' aria-labelledby="tabelLabel">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Class</th>
+                            <th>Level</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {abilities && abilities.map(ability => <tr key={ability.id}>
+                            <td>{ability.name}</td>
+                            <td>{ability.description}</td>
+                            <td>{ability.class}</td>
+                            <td>{ability.level}</td>
+                        </tr>
+                        )}
+                    </tbody>
+                </table></>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
+            : App.renderTable(this.state.characters, this.state.abilities);
+
+        return (
+            <div>
+                <h1 id="tabelLabel" >Character List</h1>
+                <p>List of characters saved in the database</p>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateCharacterData() {
+        const response = await fetch('characters');
+        const data = await response.json();
+        this.setState({ characters: data, loading: false });
+        console.log(data);
+    }
+
+    async populateAbilityData() {
+        const response = await fetch('abilities');
+        const data = await response.json();
+        this.setState({ abilities: data, loading: false });
+        console.log(data);
+    }
+}
---
+++ src/setupProxy.js
@@ -0,0 +1,16 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const context = [
+    "/weatherforecast",
+    "/characters",
+    "/abilities",
+];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: 'https://localhost:7188',
+        secure: false
+    });
+
+    app.use(appProxy);
+};
